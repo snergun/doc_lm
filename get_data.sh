ff7331855b277ad5ae5b5f3cfd875f1fea9c3282
Esen <eergun@ucsd.edu>
2026-04-20 17:50:26 +0000

    add wikitext-2 data setup

--- get_data.sh
+++ get_data.sh
@@ -11,13 +11,24 @@ wget --quiet --continue -O test.txt https://raw.githubusercontent.com/yangsaiyon
 cd ..
 
 echo "- Downloading WikiText-2 (WT2)"
-wget --quiet --continue https://s3.amazonaws.com/research.metamind.io/wikitext/wikitext-2-v1.zip
-unzip -q wikitext-2-v1.zip
+# wget --quiet --continue https://s3.amazonaws.com/research.metamind.io/wikitext/wikitext-2-v1.zip
+# unzip -q wikitext-2-v1.zip
+# cd wikitext-2
+# mv wiki.train.tokens train.txt
+# mv wiki.valid.tokens valid.txt
+# mv wiki.test.tokens test.txt
+pip install kaggle
+mkdir -p ~/.kaggle
+cp /secret/kaggle.json ~/.kaggle/kaggle.json
+chmod 600 ~/.kaggle/kaggle.json
+kaggle datasets download -d rohitgr/wikitext
+unzip -q wikitext.zip
 cd wikitext-2
 mv wiki.train.tokens train.txt
 mv wiki.valid.tokens valid.txt
 mv wiki.test.tokens test.txt
 
+cd ..
 echo "---"
 echo "Happy language modeling :)"
 
